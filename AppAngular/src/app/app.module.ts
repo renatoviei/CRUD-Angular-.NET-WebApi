f0bf090074b5ae6a290014d60d22a95acac1c643
Renato <renatoviei@gmail.com>
2021-10-03 19:14:23 -0300

    Criando model e serviço no Angular
    Foram criadas as classes model de Pessoa e PessoaService. Operações do serviços também foram implementadas.

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PessoasService } from './pessoas.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ModalModule} from "ngx-bootstrap/modal";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [  HttpClientModule, PessoasService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
