2fba25d1e081ed34d5e27d0d93508ae55eb79e33
Renato <renatoviei@gmail.com>
2021-10-04 21:31:07 -0300

    Listagem de dados
    Foi implementada a funcionalidade de listagem de dados e opções de exibição dos formulários.

--- AppAngular/src/app/components/pessoas/pessoas.component.ts
+++ AppAngular/src/app/components/pessoas/pessoas.component.ts
@@ -13,26 +13,50 @@ export class PessoasComponent implements OnInit {
 
   formulario: any;
   tituloFormulario: string;
+  pessoas : Pessoa[];
+
+  visibilidadeTabela : boolean = true;
+  visibilidadeFormulario : boolean = false; 
 
   constructor(private pessoasService: PessoasService) { }
 
   ngOnInit(): void {
 
+    this.pessoasService.PegarTodos().subscribe(resultado => {
+      this.pessoas = resultado;
+    });
+
+    
+  }
+
+  ExibirFormularioCadastro():void{
+    this.visibilidadeTabela = false;
+    this.visibilidadeFormulario = true;
     this.tituloFormulario = 'Nova Pessoa';
     this.formulario = new FormGroup({
       nome: new FormControl(null),
       sobrenome: new FormControl(null),
       idade: new FormControl(null),
       profissao: new FormControl(null)
-    });
+    });    
   }
 
   EnviarFormulario(): void{
     const pessoa : Pessoa = this.formulario.value;
 
     this.pessoasService.SalvarPessoa(pessoa).subscribe((resultado) => {
+      this.visibilidadeFormulario = false;
+      this.visibilidadeTabela = true;
       alert('Pessoa inserida com sucesso');
+      this.pessoasService.PegarTodos().subscribe((registros) => {
+        this.pessoas = registros;
+      });
     });
+  }
+
+  Voltar(): void {
+    this.visibilidadeTabela = true;
+    this.visibilidadeFormulario = false;
   }
 
 }
